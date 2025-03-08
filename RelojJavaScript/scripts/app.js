f2e730ea04a9bdde56b9efb9d4fe2e538495ebd5
LightOfStars <starcrafiteros@gmail.com>
2025-03-07 22:24:45 -0500

    Inicializando códigos base

---
+++ RelojJavaScript/scripts/app.js
@@ -0,0 +1,8 @@
+/* Ejercicio una función que permite mostrar el reloj
+en el elemento HTML con id="reloj" al presionar el botón con id="mostrar" */
+function reloj() {
+    
+}
+
+// Agregar el evento click al botón
+document.getElementById("mostrar").addEventListener("click", reloj);
